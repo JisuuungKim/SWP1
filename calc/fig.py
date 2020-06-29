b39666169c6b6000dab83c9b3a5e20156963f6db
JisuuungKim <cham0810@kookmin.ac.kr>
2020-06-30 02:21:18 +0900

    issue #4

---
+++ calc/fig.py
@@ -0,0 +1,23 @@
+from cgi import parse_qs
+from template import html
+
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', ['0'])[0]
+    b = d.get('b', ['0'])[0]
+    if ''not in [a, b]:
+        a, b = int(a), int(b)
+        x = a + b
+        y = a * b
+    else : a, b = int(a), int(b)
+    response_body = html%{
+            'sum':x,
+            'mul':y,
+    }
+
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+    <body>
+        <form action="">
+            a = <input type="number" name="a"><br><br>
+            b = <input type="number" name="b"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+        <p>
+            sum: %(sum)s</br>
+            mul: %(mul)s</br>
+        </p>
+    </body>
+</html>
+"""
